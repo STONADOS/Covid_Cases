c5e8ffce0203d321bd138a08b3fdbe078eab84d7
STONADOS <65850583+STONADOS@users.noreply.github.com>
2020-07-19 14:56:27 +0530

    Add files via upload

---
+++ covid.py
@@ -0,0 +1,24 @@
+import requests
+from bs4 import BeautifulSoup
+
+while True:
+    countryname = input("Country name: ")
+    country = countryname.lower()
+
+    url = ("https://www.worldometers.info/coronavirus/country/"+country)
+    req = requests.get(url)
+    obj = BeautifulSoup(req.text, "html.parser")
+    data = obj.find_all("div", class_ = "maincounter-number")
+
+    try:
+
+        print("Total case: ",data[0].text.strip())
+        print("Total deaths: ",data[1].text.strip())
+        print("Total recoveries: ",data[2].text.strip())
+
+    except:
+
+        print("Somthing got wrong you stupid as well as eidot!!")
+
+    if countryname == "quit":
+        exit()
